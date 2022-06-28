0040b1bf473aa4dd6ddeaa5905914df14487753f
Code Sandbox <hello@codesandbox.io>
2022-06-28 17:42:03 +0000

    Deployment from Codesandbox.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Frontend Mentor - Intro component with sign up form
+# Bert Simpson - Portfolio
 
-link to active site: https://noyiot.csb.app 
+This is the starter project for the portfolio website of Bert Simpson (name inspired by Bart Simpson).
+
+It has been created for the web development workshop at GDSC Shiv Nadar University, taking place from October 20, 2021 to November 1, 2021.
--- main.js
+++ main.js
@@ -2,43 +2,45 @@ const firstname = document.getElementById("firstn");
 const lastname = document.getElementById("lastn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const fname = document.getElementById("fnameempty");
-const lname = document.getElementById("lnameempty");
+
 const emailempty = document.getElementById("emailempty");
 const pwd = document.getElementById("pwdempty");
-
+const button = document.getElementById("btn");
+const error = document.querySelectorAll(".form_p");
 function validatefname() {
   var x = firstname.value;
-  if (x === " ") {
-    fname.classList.toggle("show");
-    return false;
+
+  if (x == "") {
+    error[0].classList.add("show");
+  } else {
+    error[0].classList.remove("show");
   }
-  return true;
 }
+
 function validatelname() {
   var y = lastname.value;
-  if (y === " ") {
-    lname.classList.toggle("show");
-    return false;
+  if (y == "") {
+    error[1].classList.add("show");
+  } else {
+    error[1].classList.remove("show");
   }
-  return true;
 }
 function validatepwd() {
   var z = password.value;
-  if (z === " ") {
-    pwd.classList.toggle("show");
+  if (z == "") {
+    pwd.style.display = "block";
     return false;
   }
   return true;
 }
-
+const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 function validateemail() {
   var a = email.value;
-  if (a === " ") {
-    emailempty.classList.toggle("show");
-    return false;
+  if (a.match(regex)) {
+    error[2].classList.remove("show");
+  } else {
+    error[2].classList.add("show");
   }
-  return true;
 }
 function validate() {
   validateemail();
@@ -46,3 +48,7 @@ function validate() {
   validatelname();
   validatefname();
 }
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  validate();
+});
